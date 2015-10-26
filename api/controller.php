e3085b32b483b144ec135064b087b99e0e3d017a
KayLee <caleboau2012@gmail.com>
2015-10-26 19:55:53 +0100

    Fix for accounts created from excel [Betty's database].
    #kaylee

--- api/controller.php
+++ api/controller.php
@@ -86,45 +86,107 @@ class controller{
         }
     }
 
+    function search($session_id, $email, $module){
+        $search_by_module_parameters = array(
+            //Session id
+            "session" => $session_id,
+
+            //The string to search for.
+            'search_string' => $email,
+
+            //The list of modules to query.
+            'modules' => array(
+                $module
+            ),
+
+            //The record offset from which to start.
+            'offset' => 0,
+
+            //The maximum number of records to return.
+            'max_results' => 10,
+
+            //Filters records by the assigned user ID.
+            //Leave this empty if no filter should be applied.
+            'id' => '',
+
+            //An array of fields to return.
+            //If empty the default return fields will be from the active listviewdefs.
+            'select_fields' => array(
+                'id',
+                'name',
+                'jjwg_maps_address_c',
+                'billing_address_state',
+                'phone_office',
+                'email1',
+                'source_c',
+                'how_c',
+            ),
+
+            //If the search is to only search modules participating in the unified search.
+            //Unified search is the SugarCRM Global Search alternative to Full-Text Search.
+            'unified_search_only' => false,
+
+            //If only records marked as favorites should be returned.
+            'favorites' => false
+        );
+
+        $model = new Model(SEARCH_BY_MODULE, $search_by_module_parameters);
+        $search_by_module_result = $model->call();
+
+//        die(var_dump($search_by_module_result));
+
+        if(empty($search_by_module_result->entry_list[0]->records)){
+            return array(
+                "status" => 0
+            );
+        }
+        else{
+            return array(
+                "status" => 1,
+                "data" => $search_by_module_result
+            );
+        }
+    }
+
     function createAccount($session_id, $name, $address, $location, $phone, $email, $how, $source){
         $exists = $this->search($session_id, $email, ACCOUNTS);
 
         if($exists["status"] == 1){
-            $exists = $this->search($session_id, $phone, ACCOUNTS);
-
-            if($exists["status"] == 1){
-//                return array(
+//            $exists = $this->search($session_id, $phone, ACCOUNTS);
+//
+//            if($exists["status"] == 1){
+////                return array(
 //                    "status" => 0,
 //                    'message' => "Account Already Exists"
 //                );
 
-                //update account -------------------------------------
-                $set_entry_parameters = array(
+            //update account -------------------------------------
+            $set_entry_parameters = array(
 
-                    //session id
-                    "session" => $session_id,
+                //session id
+                "session" => $session_id,
 
-                    //The name of the module from which to retrieve records.
-                    "module_name" => "Accounts",
+                //The name of the module from which to retrieve records.
+                "module_name" => "Accounts",
 
-                    //Record attributes
-                    "name_value_list" => array(
-                        //to update a record, you will need to pass in a record id as commented below
-                        array("name" => "id", "value" => $exists["data"]->entry_list[0]->records[0]->id->value),
-                        array("name" => "name", "value" => $name),
-                        array("name" => "jjwg_maps_address_c", "value" => $address),
-                        array("name" => "billing_address_state", "value" => $location),
-                        array("name" => "phone_office", "value" => $phone),
-                        array("name" => "email1", "value" => $email),
-                        array("name" => "source_c", "value" => $source),
-                        array("name" => "how_c", "value" => $how),
-                        array("name" => "account_type", "value" => LEAD),
-                    )
-                );
+                //Record attributes
+                "name_value_list" => array(
+                    //to update a record, you will need to pass in a record id as commented below
+                    array("name" => "id", "value" => $exists["data"]->entry_list[0]->records[0]->id->value),
+                    array("name" => "name", "value" => $name),
+                    array("name" => "jjwg_maps_address_c", "value" => $address),
+                    array("name" => "billing_address_state", "value" => $location),
+                    array("name" => "phone_office", "value" => $phone),
+                    array("name" => "email1", "value" => $email),
+                    array("name" => "source_c", "value" => $source),
+                    array("name" => "how_c", "value" => $how),
+                    array("name" => "account_type", "value" => LEAD),
+                )
+            );
 
-                $model = new Model(SET_ENTRY, $set_entry_parameters);
-                $set_entry_result = $model->call();
-            }
+            $model = new Model(SET_ENTRY, $set_entry_parameters);
+            $set_entry_result = $model->call();
+//            }
         }
         else {
             //create account -------------------------------------
@@ -155,18 +217,31 @@ class controller{
             $set_entry_result = $model->call();
         }
 
-            if (isset($set_entry_result->id)) {
-                return array(
-                    "status" => 1,
-                    'accountid' => $set_entry_result->id,
-                    'message' => "Lead successfully created"
-                );
-            } else {
-                return array(
-                    "status" => 0,
-                    'message' => "Account Creation Unsuccessful"
-                );
+        if (isset($set_entry_result->id)) {
+            return array(
+                "status" => 1,
+                'accountid' => $set_entry_result->id,
+                'message' => "Lead successfully created"
+            );
+        } else {
+            return array(
+                "status" => 0,
+                'message' => "Account Creation Unsuccessful"
+            );
+        }
+    }
+
+    function reviveAllAccounts($session_id){
+        $accounts = $this->fetchAllAccounts($session_id);
+
+        while($accounts->total_count > 0){
+            foreach($accounts->entry_list as $account){
+                $this->updateAccount($session_id, $account->id);
             }
+            $accounts = $this->fetchAllAccounts($session_id);
+        }
+
+        return true;
     }
 
     function fetchAllAccounts($session_id){
@@ -225,19 +300,6 @@ class controller{
 //        die(var_dump($accounts));
 
         return $accounts;
-    }
-
-    function reviveAllAccounts($session_id){
-        $accounts = $this->fetchAllAccounts($session_id);
-
-        while($accounts->total_count > 0){
-            foreach($accounts->entry_list as $account){
-                $this->updateAccount($session_id, $account->id);
-            }
-            $accounts = $this->fetchAllAccounts($session_id);
-        }
-
-        return true;
     }
 
     function updateAccount($session_id, $account_id){
@@ -299,68 +361,6 @@ class controller{
                 "status" => 0,
                 'message' => "Account Upgrade Unsuccessful"
             ));
-        }
-    }
-
-    function search($session_id, $email, $module){
-        $search_by_module_parameters = array(
-            //Session id
-            "session" => $session_id,
-
-            //The string to search for.
-            'search_string' => $email,
-
-            //The list of modules to query.
-            'modules' => array(
-                $module
-            ),
-
-            //The record offset from which to start.
-            'offset' => 0,
-
-            //The maximum number of records to return.
-            'max_results' => 10,
-
-            //Filters records by the assigned user ID.
-            //Leave this empty if no filter should be applied.
-            'id' => '',
-
-            //An array of fields to return.
-            //If empty the default return fields will be from the active listviewdefs.
-            'select_fields' => array(
-                'id',
-                'name',
-                'jjwg_maps_address_c',
-                'billing_address_state',
-                'phone_office',
-                'email1',
-                'source_c',
-                'how_c',
-            ),
-
-            //If the search is to only search modules participating in the unified search.
-            //Unified search is the SugarCRM Global Search alternative to Full-Text Search.
-            'unified_search_only' => false,
-
-            //If only records marked as favorites should be returned.
-            'favorites' => false
-        );
-
-        $model = new Model(SEARCH_BY_MODULE, $search_by_module_parameters);
-        $search_by_module_result = $model->call();
-
-//        die(var_dump($search_by_module_result));
-
-        if(empty($search_by_module_result->entry_list[0]->records)){
-            return array(
-                "status" => 0
-            );
-        }
-        else{
-            return array(
-                "status" => 1,
-                "data" => $search_by_module_result
-            );
         }
     }
 
